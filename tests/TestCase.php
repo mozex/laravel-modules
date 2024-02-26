a78b0d7ee4721a2f3374f2d830178ced83897757
Mohammad Zahed <mozex.gm@gmail.com>
2024-02-26 11:15:16 +0300

    fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,22 +2,12 @@
 
 namespace Mozex\Modules\Tests;
 
-use App\Providers\WorkbenchServiceProvider;
-use Livewire\LivewireServiceProvider;
-use Mozex\Modules\ModulesServiceProvider;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithViews;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    use InteractsWithViews;
     use WithWorkbench;
-
-    protected function getPackageProviders($app): array
-    {
-        return [
-            WorkbenchServiceProvider::class,
-            LivewireServiceProvider::class,
-            ModulesServiceProvider::class,
-        ];
-    }
 }
